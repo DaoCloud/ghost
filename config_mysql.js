1a7961dc374ce57f34489642e1103a045f3b65d6
Yixin Hao <haoyixin@live.cn>
2015-11-28 23:23:09 +0800

    Change back

--- config_mysql.js
+++ config_mysql.js
@@ -31,10 +31,6 @@ config = {
         },
         
         fileStorage: true,
-        
-        paths: {
-            contentPath: '/volume/ghost/content/'
-        }
     },
 
     // ### Development **(default)**
